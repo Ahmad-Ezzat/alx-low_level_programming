01a5e4a47c41538f2bd371a953f862572eb3e123
Ahmed <ahmadabdo332021@gmail.com>
2023-09-06 22:44:19 -0700

    My commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+/**
+ * main - Entry point
+ * Description: A C program that prints with put function
+ * Return: Ajways 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
