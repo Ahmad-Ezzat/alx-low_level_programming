f930906b0f5c8e2bfea1c46c0b4d29c7780efff3
Ahmed <ahmadabdo332021@gmail.com>
2023-09-08 04:42:13 -0700

    My commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print all aplhapet
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	/*prints a - z*/
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	/*prints A - Z*/
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
