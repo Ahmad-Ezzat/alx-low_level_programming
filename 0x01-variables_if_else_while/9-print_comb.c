0a4274bd96b9dbc6971cf374a1f3011f8c538529
Ahmed <ahmadabdo332021@gmail.com>
2023-09-08 05:24:20 -0700

    My commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print numbers of base16 in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+			digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
